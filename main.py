75dc317122a249298bcd9f04dae1a2dbe881ccb5
PrzemyslawJarosinski <przemyslaw.jarosinski@gmail.com>
2025-11-10 13:35:30 +0100

    Dodałem main.py z błędami z przykładu z wideo

---
+++ main.py
@@ -0,0 +1,9 @@
+def hello():
+  print("hej! w tej linii kodu jest tabulator")
+
+def bye():
+    print("żegnaj! w tej linii kodu są 4 spacje, nie jestem pewien co będzie błędem ;)")
+
+print(hello())
+
+#czy wywołanie hello to nie powinno być tylko hello() ?
