5012e40fa4eddbd00ade91325d9b4b8c681e8616
PrzemyslawJarosinski <przemyslaw.jarosinski@gmail.com>
2025-11-10 14:11:04 +0100

    Create petle.py

---
+++ petle.py
@@ -0,0 +1,7 @@
+# pierwszą napiszę dobrze, a drugą źle
+liczby = [1, 2, 3, 5, 8, 2137]
+for liczba in liczby:
+    print(f"To jest liczba {liczba}.")
+
+for liczba in liczby
+    print("To jest liczba " + liczba)
